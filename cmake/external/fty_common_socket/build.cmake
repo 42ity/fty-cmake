6f110843cc77bc3fda177f6f21ee5aa0c4802491
Dim <dmitrijzukov@eaton.com>
2020-11-25 13:17:04 +0100

    Added some other projects to external

---
+++ cmake/external/fty_common_socket/build.cmake
@@ -0,0 +1,6 @@
+
+add_dependecy(fty_common_socket
+    VERSION      "master"
+    LIB_OUTPUT   "lib/libfty_common_socket.so"
+    DEPENDENCIES cxxtools fty_common_logging fty_common
+)
---
+++ cmake/external/fty_lib_certificate/build.cmake
@@ -0,0 +1,5 @@
+
+add_dependecy(fty_lib_certificate
+    VERSION      "master"
+    LIB_OUTPUT   "lib/libfty_lib_certificate.so"
+)
